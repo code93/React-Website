e5cd0225cc559957667b5a545191f4f1a51161f1
shubham birmi <39634733+code93@users.noreply.github.com>
2019-05-03 20:55:43 +0530

    React JS Start code snippet
    React Js code snippet for creating components and HOC 's, to add css stylesheet by npm run eject...

---
+++ src/components/Header/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './header.css'
+
+const Header = () => {
+
+    return (
+        <header className={style.header}>
+            <div className={style.headerOpt}>
+            header
+            </div>
+        </header>
+    )
+}
+
+export default Header;
