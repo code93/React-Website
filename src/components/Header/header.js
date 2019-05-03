79cb0c651cd0cc9333a66be4363a2176716d41a1
shubham birmi <39634733+code93@users.noreply.github.com>
2019-05-03 23:30:05 +0530

    Header Logo and layout
    Header having a Copyrighted Logo of FIM with a navigation bar using Font Awesome,...

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import style from './header.css'
+import style from './header.css';
+import { Link } from 'react-router-dom';
+
+import FontAwesome from 'react-fontawesome';
 
 const Header = () => {
+
+
+    const navBars = () => (
+        <div className={style.bars}>
+            <FontAwesome name="bars"
+            
+                style={{
+                    color: '#dfdfdf',
+                    padding: '10px',
+                    cursor: 'pointer'
+                }}
+            />
+        </div>
+    )
+
+    const logo = () =>{
+        return (
+            <Link to="/" className={style.logo}>
+                <img alt="fim logo" src="/images/fim_logo.png"/>
+            </Link>
+        )
+    }
+
 
     return (
         <header className={style.header}>
             <div className={style.headerOpt}>
-            header
+                {navBars()}
+                {logo()}
             </div>
         </header>
     )
