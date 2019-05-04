18fb00275f685ff2a0ecd70175ee0caacbf011fd
shubham birmi <39634733+code93@users.noreply.github.com>
2019-05-04 12:00:19 +0530

    SideNav Added
    Functions used to open SideNav and a little styling done

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -3,14 +3,15 @@ import style from './header.css';
 import { Link } from 'react-router-dom';
 
 import FontAwesome from 'react-fontawesome';
+import SideNav from './SideNav/sideNav';
 
-const Header = () => {
+const Header = (props) => {
 
 
     const navBars = () => (
         <div className={style.bars}>
             <FontAwesome name="bars"
-            
+                onClick={props.onOpenNav}
                 style={{
                     color: '#dfdfdf',
                     padding: '10px',
@@ -31,6 +32,7 @@ const Header = () => {
 
     return (
         <header className={style.header}>
+            <SideNav {...props}/>
             <div className={style.headerOpt}>
                 {navBars()}
                 {logo()}
