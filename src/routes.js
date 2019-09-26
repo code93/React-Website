d1964629cdc6e091320c3c72f36b879763160f67
shubham birmi <39634733+code93@users.noreply.github.com>
2019-09-26 18:36:48 +0530

    Article Post view
    Half done

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Home from './components/Home'
+import Home from './components/Home';
 
-import Layout from './hoc/Layout/layout'
+import Layout from './hoc/Layout/layout';
+
+import NewsArticle from './components/Articles/News/Post/index';
+
 
 class Routes extends Component {
     render(){
@@ -10,6 +13,7 @@ class Routes extends Component {
             <Layout>
                 <Switch>
                     <Route path="/" exact component={Home}/> 
+                    <Route path="/articles/:id" exact component={NewsArticle}/>
                 </Switch>
             </Layout>
             
