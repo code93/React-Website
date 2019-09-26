454b8b174c3527332fe35cd493e5a4e720f587cc
shubham birmi <39634733+code93@users.noreply.github.com>
2019-09-26 18:03:47 +0530

    videos(ii)

--- src/components/widgets/VideosList/videosList.js
+++ src/components/widgets/VideosList/videosList.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 import { URL } from '../../../config';
 import Button from '../Buttons/buttons';
-
+import VideosListTemplate from './videosListTemplate'
 
 class VideosList extends Component {
 
@@ -16,8 +16,44 @@ class VideosList extends Component {
         amount: this.props.amount
     }
 
+    componentWillMount(){
+        this.request(this.state.start, this.state.end)
+    }
+
+    request = (start, end) => {
+        if(this.state.teams.length < 1) {
+            axios.get(`${URL}/teams`)
+            .then( response => {
+                this.setState({
+                    teams:response.data
+                })
+            })
+        }
+
+        axios.get(`${URL}/videos?_start=${start}&_end=${end}`)
+        .then( response => {
+            this.setState({
+                videos: [...this.state.videos,...response.data]
+            })
+        })
+
+    }
+
+    renderVideos = () => {
+        let template = null;
+
+        switch(this.props.type){
+            case('card'):
+                template = <VideosListTemplate data={this.state.videos} teams={this.state.teams}/>
+            break;
+            default:
+                template = null
+        }
+        return template;
+    }
+
     loadMore = () => {
-        
+
     }
 
     renderButton = () => {
@@ -42,6 +78,7 @@ render(){
     return(
         <div className={styles.videoList_wrapper}>
             { this.renderTitle() }
+            { this.renderVideos() }
             { this.renderButton() }
         </div>
     )
