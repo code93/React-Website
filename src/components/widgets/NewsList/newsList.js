d36747d4411df56a17e1f317952d9d3593698a36
shubham birmi <39634733+code93@users.noreply.github.com>
2019-09-26 16:57:55 +0530

    Added newsList teams
    dummy data

--- src/components/widgets/NewsList/newsList.js
+++ src/components/widgets/NewsList/newsList.js
@@ -6,10 +6,13 @@ import axios from 'axios';
 import { URL } from '../../../config';
 import styles from './newsList.css';
 import Button from '../Buttons/buttons';
+import CardInfo from '../../widgets/CardInfo/cardinfo';
+
 
 class NewsList extends Component {
 
     state = {
+        teams: [],
         items: [],
         start:this.props.start,
         end:this.props.start + this.props.amount,
@@ -21,6 +24,16 @@ class NewsList extends Component {
     }
 
     request = (start,end) => {
+        if(this.state.teams.length < 1){
+            axios.get(`${URL}/teams`)
+            .then( response => {
+                this.setState({
+                    teams: response.data
+                })
+            })
+        }
+
+
         axios.get(`${URL}/articles?_start=${start}&_end=${end}`)
         .then( response => {
             this.setState({
@@ -49,8 +62,9 @@ class NewsList extends Component {
                         key={i}
                     >
                          <div>
-                        <div className={StyleSheet.newslist_item}>
+                        <div className={styles.newslist_item}>
                             <Link to={`/articles/${item.id}`}>
+                                <CardInfo teams={this.state.teams} team={item.team} date={item.date}/>
                                 <h2>{item.title}</h2>
                             </Link>
                         </div>
