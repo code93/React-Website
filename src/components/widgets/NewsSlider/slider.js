3839ab6f0830790cb9d024ae643f7a507fb0a122
shubham birmi <39634733+code93@users.noreply.github.com>
2019-10-05 20:23:04 +0530

    Slider Fixed

--- src/components/widgets/NewsSlider/slider.js
+++ src/components/widgets/NewsSlider/slider.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { firebaseArticles, firebaseLooper } from '../../../firebase';
+import {firebase, firebaseArticles, firebaseLooper } from '../../../firebase';
 
 import SliderTemplates from './slider_templates';
+import { resolve } from 'url';
 
 
 class NewsSlider extends Component {
@@ -13,14 +14,42 @@ class NewsSlider extends Component {
         componentWillMount(){
             firebaseArticles.limitToFirst(3).once('value')
             .then((snapshot)=>{
-                  
                 const news= firebaseLooper(snapshot)
-                  this.setState({
-                      news
-                  })
 
+                // news.forEach((item,i)=>{
+                //     firebase.storage().ref('images')
+                //     .child(item.image).getDownloadURL()
+                //     .then( url =>{
+                //         news[i].image =url;
+                //         this.setState({
+                //             news
+                //         })
+                //     })
+                // });
 
-            })
+                const asyncFunction = (item,i,cb) => {
+                    firebase.storage().ref('images')
+                    .child(item.image).getDownloadURL()
+                    .then(url => {
+                        news[i].image = url;
+                       cb();
+                    })
+                }
+
+                //let request = []
+                let requests = news.map((item,i) =>{
+                    return new Promise((resolve)=> {
+                        asyncFunction(item,i, resolve)
+                    })
+                })
+
+                Promise.all(requests).then(()=>{
+                    this.setState({
+                        news
+                    })
+                })
+
+            });
            /*  axios.get(`${URL}/articles?_start=${this.props.start}&_end=${this.props.amount}`)
             .then( response => {
                 this.setState({
