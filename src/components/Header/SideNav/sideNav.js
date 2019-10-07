b75d0494be84bd0cc44d734073b4b426e4dc31c4
shubham birmi <39634733+code93@users.noreply.github.com>
2019-10-07 17:21:50 +0530

    sideNav fixed

--- src/components/Header/SideNav/sideNav.js
+++ src/components/Header/SideNav/sideNav.js
@@ -9,7 +9,7 @@ const SideNavigation = (props) => {
                 <SideNav
                     showNav={props.showNav}
                     onHideNav={props.onHideNav}
-                    navstyle={{
+                    navStyle={{
                         background: '#242424',
                         maxWidth: '220px'
                     }}
